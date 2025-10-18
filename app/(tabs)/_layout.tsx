3f8dc258f29bcc63a3d2db58426a58347c7cad9c
Carles <c.delriofrances@gmail.com>
2025-10-18 17:32:49 +0200

    dark mode updating

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,50 @@
-import { Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/4-shared/components/haptic-tab';
-import { IconSymbol } from '@/4-shared/components/ui/icon-symbol';
-import { useColorScheme } from '@/4-shared/hooks/use-color-scheme';
-import { useTheme } from '@/4-shared/theme/ThemeProvider';
+import { HapticTab } from "@/4-shared/components/haptic-tab";
+import { IconSymbol } from "@/4-shared/components/ui/icon-symbol";
+import { useTheme } from "@/4-shared/theme/ThemeProvider";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  const { mode, theme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: theme.accent, 
+        tabBarActiveTintColor: theme.accent,
+        tabBarStyle: {
+          backgroundColor: theme.background, // <--- This line makes your tab bar themed!
+        },
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color }) => (
-            <IconSymbol type="sf" size={28} name="house.fill" color={color} accessibilityLabel="Home Tab" />
+            <IconSymbol
+              type="sf"
+              size={28}
+              name="house.fill"
+              color={color}
+              accessibilityLabel="Home Tab"
+            />
           ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: "Explore",
           tabBarIcon: ({ color }) => (
-            <IconSymbol type="sf" size={28} name="paperplane.fill" color={color} accessibilityLabel="Explore Tab" />
+            <IconSymbol
+              type="sf"
+              size={28}
+              name="paperplane.fill"
+              color={color}
+              accessibilityLabel="Explore Tab"
+            />
           ),
         }}
       />
