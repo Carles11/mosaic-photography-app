f5349eaf2a50c84eecf7b24c4dd4bafc3ed91112
Carles <c.delriofrances@gmail.com>
2025-10-23 11:42:59 +0200

    updated tabs

---
+++ app/(tabs)/collections.tsx
@@ -0,0 +1,6 @@
+import CollectionsList from "@/1-pages/collections/collections-list/ui/CollectionsList";
+import React from "react";
+
+export default function CollectionsListScreen() {
+  return <CollectionsList />;
+}
