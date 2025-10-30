b1d544c0ed6434b27ff01a97c8b30e5d62929c22
Carles <c.delriofrances@gmail.com>
2025-10-30 12:16:26 +0100

    keyboard hiding fixes

--- src/2-features/favorites-list/ui/AddToCollectionSheet.styles.ts
+++ src/2-features/favorites-list/ui/AddToCollectionSheet.styles.ts
@@ -26,4 +26,28 @@ export const styles = {
   listContent: {
     paddingBottom: Platform.OS === "android" ? 48 : 0,
   },
+  createNewButton: {
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  createForm: {
+    marginTop: 8,
+    marginBottom: 8,
+    paddingHorizontal: 4,
+  },
+  input: {
+    padding: 8,
+    borderRadius: 6,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    marginBottom: 6,
+    fontSize: 16,
+  },
+  createButton: {
+    marginBottom: 6,
+  },
+  cancelButton: {
+    marginBottom: 4,
+  },
 };
