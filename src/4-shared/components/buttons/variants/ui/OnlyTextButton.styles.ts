65224d2067cdafc4f1b97abc17bbac5fa02f9b58
Carles <c.delriofrances@gmail.com>
2025-11-03 22:02:23 +0100

    done styling reports

--- src/4-shared/components/buttons/variants/ui/OnlyTextButton.styles.ts
+++ src/4-shared/components/buttons/variants/ui/OnlyTextButton.styles.ts
@@ -2,12 +2,10 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   button: {
-    paddingTop: 10,
     paddingHorizontal: 20,
     borderWidth: 0,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 6,
     minWidth: 80,
     backgroundColor: "transparent",
   },
