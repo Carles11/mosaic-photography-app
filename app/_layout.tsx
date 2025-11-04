fb76d51e301b68818bf38a454d34656301d53ea0
Carles <c.delriofrances@gmail.com>
2025-11-04 20:51:56 +0100

    about to move folder to C

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,22 +16,14 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
   sendDefaultPii: true,
-
-  // Enable Logs
   enableLogs: true,
-
-  // Configure Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
   integrations: [
     Sentry.mobileReplayIntegration(),
     Sentry.feedbackIntegration(),
   ],
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
 });
 
