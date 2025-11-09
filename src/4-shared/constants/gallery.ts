d454631801c779b65d28fb834301697385fc2954
Carles <c.delriofrances@gmail.com>
2025-11-09 18:22:50 +0100

    main gallery height fix

--- src/4-shared/constants/gallery.ts
+++ src/4-shared/constants/gallery.ts
@@ -3,6 +3,6 @@ export const GALLERY_ITEM_HEIGHT = 480;
 // Calculate image height based on fixed cell height minus header (50), title (33), footer (30)
 const IMAGE_HEADER_HEIGHT = 50;
 const TITLE_HEIGHT = 33;
-const FOOTER_ROW_HEIGHT = 30;
+const FOOTER_ROW_HEIGHT = 50;
 export const IMAGE_HEIGHT =
   GALLERY_ITEM_HEIGHT - IMAGE_HEADER_HEIGHT - TITLE_HEIGHT - FOOTER_ROW_HEIGHT;
